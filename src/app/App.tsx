02d18b8ec6ab0123cd5541f4955a5e0db780c130
egoloprum <bayraa093@gmail.com>
2026-04-07 02:22:37 +0300

    feature: header UI and routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Favorites, Home } from './routes'
+import { Header } from '../widgets/header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
